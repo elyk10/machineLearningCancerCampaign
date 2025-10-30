5fb544a9d0169637217e02eaeb24c919f4115c39
elyk10 <kweb@rogers.com>
2025-10-30 15:14:39 -0400

    Finished first assignment with visuals

--- main.py
+++ main.py
@@ -29,7 +29,19 @@ def main():
     df.to_csv("data_edit.csv", index = False)
 
     sns.pairplot(df, vars = df.columns[1:6], hue = "diagnosis", diag_kind= "kde")
-    plt.suptitle("Pair Plot for select Features")
+    #plt.suptitle("Pair Plot for select Features")
+    #plt.show()
+
+    plt.figure(figsize = (12, 8))
+    sns.heatmap(df.corr(), cmap = "coolwarm", annot = False)
+    plt.title("Correlation Heatmap")
+    plt.show()
+
+    plt.figure(figsize = (20, 10))
+    dfMelted = df.melt(id_vars = "diagnosis", var_name = "Feature", value_name = "Value")
+    sns.boxplot(x = "Feature", y = "Value", hue = "diagnosis", data = dfMelted)
+    plt.xticks(rotation = 90)
+    plt.title("Box Plots of Features by Diagnosis")
     plt.show()
 
 main()
